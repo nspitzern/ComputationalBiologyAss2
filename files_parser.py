110cd9eec162c5dfdafd6b9d2671ded442654557
Nadav Spitzer <nspitzern@gmail.com>
2023-05-08 22:31:48 +0300

    Chore: Remove punctuations from enc words. Lower all parsed data.

--- files_parser.py
+++ files_parser.py
@@ -1,34 +1,37 @@
-from typing import List, Dict
+from typing import List, Dict, Tuple
 from string import punctuation, whitespace
 
 
 def parse_dict(file_path: str) -> List[str]:
     with open(file_path, 'r', encoding='utf-8') as f:
-        d = [word.strip() for word in f.readlines()]
+        d = [word.lower().strip() for word in f.readlines()]
 
     return d
 
 
 def parse_letters_freq(file_path: str) -> Dict[str, float]:
     with open(file_path, 'r', encoding='utf-8') as f:
-        d = {word.split('\t')[1].strip(): float(word.split('\t')[0].strip())
+        d = {word.split('\t')[1].lower().strip(): float(word.split('\t')[0].strip())
              for word in f.readlines() if word not in whitespace and word != '\t#REF!\n'}
 
     return d
 
 
-def parse_encoded(file_path: str) -> List[str]:
+def parse_encoded(file_path: str) -> Tuple[List[str], List[str]]:
     letters_enc = []
     words_enc = []
+    # original_enc = []
 
     with open(file_path, 'r', encoding='utf-8') as f:
         for line in f.readlines():
             if line in whitespace:
+                # original_enc.append(line)
                 continue
 
-            words = line.split(' ')
+            words = line.lower().split(' ')
 
             for word in words:
+                word = word.translate(str.maketrans('', '', punctuation))
                 word = word.strip()
                 words_enc.append(word)
 
@@ -37,4 +40,4 @@ def parse_encoded(file_path: str) -> List[str]:
                         continue
                     letters_enc.append(l)
 
-    return letters_enc
+    return words_enc, letters_enc
--- main.py
+++ main.py
@@ -5,11 +5,11 @@ from generator import Generator
 
 
 if __name__ == '__main__':
-    enc = parse_encoded('data/enc.txt')
+    enc_words, enc_letters = parse_encoded('data/enc.txt')
     dictionary = parse_dict('data/dict.txt')
     freq_1_letter = parse_letters_freq('data/Letter_Freq.txt')
     freq_2_letter = parse_letters_freq('data/Letter2_Freq.txt')
 
-    g = Generator(len(enc))
+    g = Generator(len(enc_letters))
 
     pprint(g.generate_random(50))
