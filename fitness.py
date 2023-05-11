522f56efcef327a48719c6aeaced4303d184e3f0
Nadav Spitzer <nspitzern@gmail.com>
2023-05-11 17:48:16 +0300

    Chore: Add some docs and TODO

--- fitness.py
+++ fitness.py
@@ -2,6 +2,12 @@ from typing import List
 
 
 def check_words_in_dict_ratio(dec: List[str], dictionary: List[str]) -> float:
+    """
+
+    :param dec: List of decoded words
+    :param dictionary: List of Corpus words
+    :return:
+    """
     dec = set(dec)
     dictionary = set(dictionary)
 
@@ -9,3 +15,5 @@ def check_words_in_dict_ratio(dec: List[str], dictionary: List[str]) -> float:
     n_words_intersection = len(dictionary & dec)
 
     return 100 * (n_words_intersection / n_words_dec)
+
+# TODO: 1. fitness function by similar letters. 2. fitness function by correctness of mapping
