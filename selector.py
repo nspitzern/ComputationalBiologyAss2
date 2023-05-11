70f8c5896678cecc91d61d3a7dacdff15313ae78
Nadav Spitzer <nspitzern@gmail.com>
2023-05-11 17:16:10 +0300

    Feature: Add selector

---
+++ selector.py
@@ -0,0 +1,16 @@
+from typing import List
+from math import floor
+from generator import Sample
+
+
+class Selector:
+    def __init__(self):
+        pass
+
+    def select_elite(self, samples, fitness_scores, percentile: float) -> List[Sample]:
+        selected_samples = []
+        ratio = floor(len(fitness_scores) - percentile * len(fitness_scores))  # how many samples to return
+
+        # sort the
+        samples, fitness_scores = zip(*sorted(zip(samples, fitness_scores), key=lambda x: x[1]))
+        return samples[len(samples) - ratio:]
