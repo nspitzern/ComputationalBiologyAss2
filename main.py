88002c29f90ae78685bb04f7dbc1269cfb273ff5
shimoncohen <shimoncohen655@gmail.com>
2023-05-20 23:47:52 +0300

    chore: use simulation args in main

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from src.simulator import Simulator
-
+from src.simulator import Simulator, SimulationArgs
 
 if __name__ == '__main__':
-    simulator: Simulator = Simulator(10_000, 0.95, mutation_percentage=0.5, elite_percentage=0.75)
-    simulator.run()
+    args: SimulationArgs = SimulationArgs(1, elite_percentile=0.1, mutation_percentage=0.75, 
+                                          mutation_decay=1e-3, mutation_min_percentage=0.05, 
+                                          generation_tolerance=10, generation_tolerance_percentage=0.05)
+    simulator: Simulator = Simulator(2000, args)
+
+    dict = {}
+    simulator.run(dict)
